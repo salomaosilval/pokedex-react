a8d2aee53da2d2806940b9a160a5a9a42ca61c82
Salomão Silva <salomaosilval@hotmail.com>
2023-03-05 18:26:06 -0400

    :sparkles: feat: add all components on main Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import "./App.css";
+import { useState, useEffect } from "react";
+
+import { Navbar } from "./components/Navbar/Navbar";
+import { Pokedex } from "./components/Pokedex/Pokedex";
+import { Searchbar } from "./components/Searchbar/Searchbar";
+
+import { getPokemonData, getPokemons } from "./api";
+
+import "./App.scss";
 
 function App() {
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [pokemons, setPokemons] = useState([]);
+
+  const itensPerPage = 25;
+
+  const fetchPokemons = async () => {
+    try {
+      setLoading(true);
+
+      const data = await getPokemons(itensPerPage, itensPerPage * page);
+      const promises = data.data.results.map(async (pokemon) => {
+        return await getPokemonData(pokemon.url);
+      });
+
+      const results = await Promise.all(promises);
+
+      setPokemons(results);
+      setLoading(false);
+      setTotalPages(Math.ceil(data.data.count / itensPerPage));
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [page]);
+
   return (
-    <div className="App">
-      <p>teste</p>
+    <div>
+      <Navbar />
+      <Searchbar />
+      <Pokedex pokemons={pokemons} loading={loading} page={page} totalPages={totalPages} setPage={setPage} />
     </div>
   );
 }
